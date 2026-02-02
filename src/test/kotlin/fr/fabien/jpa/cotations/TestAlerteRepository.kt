3e5a8a34832f845c4dbd8b8431f39a95ad33589b
fabien <fabien.rigaux@free.fr>
2026-02-02 17:23:51 +0100

    refactoring

--- src/test/kotlin/fr/fabien/jpa/cotations/TestAlerteRepository.kt
+++ src/test/kotlin/fr/fabien/jpa/cotations/TestAlerteRepository.kt
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.dao.InvalidDataAccessApiUsageException
-import java.util.regex.Pattern
 import kotlin.test.BeforeTest
 
 
@@ -24,20 +23,18 @@ class TestAlerteRepository(
 ) {
 
     companion object {
-        var valeurGLE: Valeur? = null
-        var alerte: Alerte? = null
+        lateinit var valeurGLE: Valeur
     }
 
     @BeforeTest
     fun setup() {
         valeurGLE = Valeur("GLE", Marche.EURO_LIST_A, "Societe Generale", setOf())
-        repositoryValeur.save(valeurGLE!!)
+        repositoryValeur.save(valeurGLE)
 
-        alerte = Alerte(
-            valeurGLE!!, "la clôture est supérieure à la MM50 et la variation du cours supérieure à 2%",
+        repositoryAlerte.save(Alerte(
+            valeurGLE, "la clôture est supérieure à la MM50 et la variation du cours supérieure à 2%",
             TypeAlerte.LIBRE, "(CLOTURE(1) > MOYENNE_MOBILE(50)) && (VARIATION(1,2) > 0.02)", null, false, TypeNotification.SYSTEME
-        )
-        repositoryAlerte.save(alerte!!)
+        ))
     }
 
     @Test
@@ -55,50 +52,50 @@ class TestAlerteRepository(
     fun `Given x Alerte dont la condition ne correspond pas au type attendu when save then InvalidDataAccessApiUsageException`() {
         assertThrows(InvalidDataAccessApiUsageException::class.java, {
             repositoryAlerte.save(Alerte(
-                valeurGLE!!, "n'importe quoi",
+                valeurGLE, "n'importe quoi",
                 TypeAlerte.SEUIL_BAS, "CLOTURE(1) > 22.2", null, false, TypeNotification.SYSTEME
             ))
         })
         assertThrows(InvalidDataAccessApiUsageException::class.java, {
             repositoryAlerte.save(Alerte(
-                valeurGLE!!, "n'importe quoi",
+                valeurGLE, "n'importe quoi",
                 TypeAlerte.SEUIL_HAUT, "CLOTURE(1) < 22.2", null, false, TypeNotification.SYSTEME
             ))
         })
         assertThrows(InvalidDataAccessApiUsageException::class.java, {
             repositoryAlerte.save(Alerte(
-                valeurGLE!!, "n'importe quoi",
+                valeurGLE, "n'importe quoi",
                 TypeAlerte.VARIATION, "VARIATION(1,3) > 2.25", null, false, TypeNotification.SYSTEME
             ))
         })
 
         assertThrows(InvalidDataAccessApiUsageException::class.java, {
             repositoryAlerte.save(Alerte(
-                valeurGLE!!, "n'importe quoi",
+                valeurGLE, "n'importe quoi",
                 TypeAlerte.TUNNEL, "CLOTURE(1) < 22.2 || CLOTURE(1) < 25.2", null, false, TypeNotification.SYSTEME
             ))
         })
         assertThrows(InvalidDataAccessApiUsageException::class.java, {
             repositoryAlerte.save(Alerte(
-                valeurGLE!!, "n'importe quoi",
+                valeurGLE, "n'importe quoi",
                 TypeAlerte.CROISEMENT_MM_BAS, "CLOTURE(1) > MOYENNE_MOBILE(50)", null, false, TypeNotification.SYSTEME
             ))
         })
         assertThrows(InvalidDataAccessApiUsageException::class.java, {
             repositoryAlerte.save(Alerte(
-                valeurGLE!!, "n'importe quoi",
+                valeurGLE, "n'importe quoi",
                 TypeAlerte.CROISEMENT_MM_HAUT, "CLOTURE(1) < MOYENNE_MOBILE(50)", null, false, TypeNotification.SYSTEME
             ))
         })
         assertThrows(InvalidDataAccessApiUsageException::class.java, {
             repositoryAlerte.save(Alerte(
-                valeurGLE!!, "n'importe quoi",
+                valeurGLE, "n'importe quoi",
                 TypeAlerte.PLUS_HAUT, "CLOTURE(1) > PLUS_HAUT(50)", null, false, TypeNotification.SYSTEME
             ))
         })
         assertThrows(InvalidDataAccessApiUsageException::class.java, {
             repositoryAlerte.save(Alerte(
-                valeurGLE!!, "n'importe quoi",
+                valeurGLE, "n'importe quoi",
                 TypeAlerte.PLUS_BAS, "CLOTURE(1) < PLUS_BAS(50)", null, false, TypeNotification.SYSTEME
             ))
         })
@@ -108,7 +105,7 @@ class TestAlerteRepository(
     fun `Given 1 Alerte dont la condition de type LIBRE est erronée when save then InvalidDataAccessApiUsageException`() {
         assertThrows(InvalidDataAccessApiUsageException::class.java, {
             repositoryAlerte.save(Alerte(
-                valeurGLE!!, "n'importe quoi",
+                valeurGLE, "n'importe quoi",
                 TypeAlerte.LIBRE, "NIMPORTEQUOI", null, false, TypeNotification.SYSTEME
             ))
         })
